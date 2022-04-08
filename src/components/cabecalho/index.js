1eb1f99a5d494c3b4ccf461e685f187f1fd54ecf
unknown <you@example.com>
2022-04-08 16:54:38 -0300

    Trabalhando com FlaList

--- src/components/cabecalho/index.js
+++ src/components/cabecalho/index.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import {View,Image,Text,TouchableOpacity,StyleSheet} from 'react-native'
+import React,{Fragment} from 'react';
+import {View,Image,Text,TouchableOpacity,StyleSheet} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 
+export default function Botao({Logo,texto,cor,Logo2,texto2,cor2}) {
 
-export default function Cabecalho(){
     return(
+        <View style={estilo.containerBotoes1}>
+        <TouchableOpacity style={[estilo.botoes ,{backgroundColor:cor}]}>
+        <Ionicons name= {Logo} size={20} color="white"
+        
+        style= {{marginRight:10}}/>
+        <Text style ={estilo.textoBotao} > {texto} </Text>
 
-        <View style={estilo.header}>
-            <Ionicons name="menu" size={24} color="white"/>
-            <Text style ={estilo.Texto}> Play store </Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={[estilo.botoes ,{backgroundColor:cor2}]}>
+        <Ionicons name= {Logo2} size={20} color="white"
+        
+        style= {{marginRight:10}}/>
+        <Text style ={estilo.textoBotao}> {texto2} </Text>
 
-            <Ionicons name="ios-search-sharp" size={24} color="white"/>
+        </TouchableOpacity>
         </View>
-
     );
 }
-    const estilo = StyleSheet.create( {
-        header: {
-            backgroundColor:'#002333',
-            borderRadius:2,
-            marginTop:8,
-            width:'99,9%',
-            padding:20,
-            margin:2,
-            alignItems:'center',
-            flexDirection:'row',
-            justifyContent: 'space-between',
-            marginBottom:20
-        },
-        Texto:{
-            color:'white',
-            marginLeft:-120,
-            fontSize:20,
-            fontWeight:'bold'
-        }
-    })
+const estilo = StyleSheet.create({
+    
+    containerBotoes1:{
+        flexDirection:'row',
+        marginBottom:10,
+        justifyContent:"center"
+    },
+    textoBotao:{
+        color:'white',
+        fontWeight:'bold'
+    },
+    botoes:{
+        padding:5,
+        marginHorizontal:10,
+        width:'45%',
+        alignItems:'center',
+        justifyContent: 'flex-start',
+
+        fontSize:12,
+        color:'white',
+        flexDirection:'row',
+    }
+})
